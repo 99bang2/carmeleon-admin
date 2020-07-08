e49f7ff4556f29456834bf363d6af80d36b5ad2d
JaegyuJeong <h923313@gmail.com>
2020-07-08 19:16:28 +0900

    ..

--- pages/tutorials.vue
+++ pages/tutorials.vue
@@ -13,94 +13,27 @@
 						</ScCardHeader>
 						<ScCardBody style="min-height: 745px">
 							<form class="uk-flex uk-flex-center uk-flex-between">
-								<div class="uk-card uk-width-1-6" style="min-height: 600px">
-									<div class="uk-text-center uk-card-header sc-theme-bg-dark sc-light">
-										<h3 class="uk-card-title">
-											step1
-										</h3>
-									</div>
-									<div class="uk-card-body uk-margin" uk-margin>
-										<div uk-form-custom="target: true">
-											<input type="file" accept="image/*" ref="step1Image" @change="onChangeStep1ImageFile">
-											<input class="uk-input uk-background-muted uk-padding uk-panel" type="text"
-												   placeholder="Select file" disabled>
+								<div data-uk-sortable="handle: .uk-card">
+									<div v-for="(stepImage, index) in sendData.stepImages" :key="index" class="uk-card uk-width-1-6" style="min-height: 600px">
+										<div class="uk-text-center uk-card-header sc-theme-bg-dark sc-light">
+											<h3 class="uk-card-title">
+												step{{index + 1}}
+											</h3>
 										</div>
-										<div class="uk-width-1-1 image-preview" v-if="sendData.step1Image">
-											<img class="preview" :src="step1Image"/>
-										</div>
-									</div>
-								</div>
-								<div class="uk-card uk-width-1-6" style="min-height: 600px">
-									<div class="uk-text-center uk-card-header sc-theme-bg-dark sc-light">
-										<h3 class="uk-card-title">
-											step2
-										</h3>
-									</div>
-									<div class="uk-card-body uk-margin" uk-margin>
-										<div uk-form-custom="target: true">
-											<input type="file" accept="image/*" ref="step2Image" @change="onChangeStep2ImageFile">
-											<input class="uk-input uk-form-width-medium" type="text"
-												   placeholder="Select file" disabled>
-										</div>
-										<div class="uk-width-1-1 image-preview" v-if="sendData.step2Image">
-											<img class="preview" :src="step2Image"/>
-										</div>
-									</div>
-								</div>
-								<div class="uk-card uk-width-1-6" style="min-height: 600px">
-									<div class="uk-text-center uk-card-header sc-theme-bg-dark sc-light">
-										<h3 class="uk-card-title">
-											step3
-										</h3>
-									</div>
-									<div class="uk-card-body uk-margin" uk-margin>
-										<div uk-form-custom="target: true">
-											<input type="file" accept="image/*" ref="step3Image" @change="onChangeStep3ImageFile">
-											<input class="uk-input uk-form-width-medium" type="text"
-												   placeholder="Select file" disabled>
-										</div>
-										<div class="uk-width-1-1 image-preview" v-if="sendData.step3Image">
-											<img class="preview" :src="step3Image"/>
-										</div>
-									</div>
-								</div>
-								<div class="uk-card uk-width-1-6" style="min-height: 600px">
-									<div class="uk-text-center uk-card-header sc-theme-bg-dark sc-light">
-										<h3 class="uk-card-title">
-											step4
-										</h3>
-									</div>
-									<div class="uk-card-body uk-margin" uk-margin>
-										<div uk-form-custom="target: true">
-											<input type="file" accept="image/*" ref="step4Image" @change="onChangeStep4ImageFile">
-											<input class="uk-input uk-form-width-medium" type="text"
-												   placeholder="Select file" disabled>
-										</div>
-										<div class="uk-width-1-1 image-preview" v-if="sendData.step4Image">
-											<img class="preview" :src="step4Image"/>
-										</div>
-									</div>
-								</div>
-								<div class="uk-card uk-width-1-6" style="min-height: 600px">
-									<div class="uk-text-center uk-card-header sc-theme-bg-dark sc-light">
-										<h3 class="uk-card-title">
-										step5
-										</h3>
-									</div>
-									<div class="uk-card-body uk-margin" uk-margin>
-										<div uk-form-custom="target: true">
-											<input type="file" accept="image/*" ref="step5Image" @change="onChangeStep5ImageFile">
-											<input class="uk-input uk-form-width-medium" type="text"
-												   placeholder="Select file" disabled>
-										</div>
-										<div class="uk-width-1-1 image-preview" v-if="sendData.step5Image">
-											<img class="preview" :src="step5Image"/>
+										<div class="uk-card-body uk-margin" uk-margin>
+											<div uk-form-custom="target: true">
+												<input type="file" accept="image/*" ref="step5Image" @change="onChangeStepImageFile($event, index)">
+												<input class="uk-input uk-form-width-medium" type="text" placeholder="Select file" disabled>
+												<div class="uk-width-1-1 image-preview" v-if="stepImage">
+													<img class="preview" :src="stepImage"/>
+												</div>
+											</div>
 										</div>
 									</div>
 								</div>
 							</form>
 							<div class="uk-margin-top uk-text-center">
-								<button class="sc-button sc-button-primary">
+								<button class="sc-button sc-button-primary" @click.prevent="submit">
 									등록
 								</button>
 							</div>
@@ -117,91 +50,38 @@
 
 	export default {
 		components: {ScCard},
-		data(){
-			return{
-				sendData:{},
-				defaultForm:{
-					step1Image:'',
-					step2Image:'',
-					step3Image:'',
-					step4Image:'',
-					step5Image:''
-				}
+		data() {
+			return {
+				sendData: {
+					stepImages: new Array(5)
+				},
+				stepImageFiles: new Array(5)
 			}
 		},
 		async created() {
-			let vm = this
-		// 	let res= await this.$axios.$get(this.config.apiUrl + '/api/tutorials')
-			vm.settingForm()
+			// 	let res= await this.$axios.$get(this.config.apiUrl + '/api/tutorials')
+			//vm.settingForm()
+			console.log(this.sendData.stepImages)
+
 		},
-		methods:{
-			settingForm(props){
-				this.step1Image =''
-				this.step2Image =''
-				this.step3Image =''
-				this.step4Image =''
-				this.step5Image =''
-				if(props){
-					this.sendData = JSON.parse(JSON.stringify(props))
-				}else {
-					this.sendData = JSON.parse(JSON.stringify(this.defaultForm))
-				}
-			},
-			onChangeStep1ImageFile(event) {
-				var input = event.target;
-				if(input.files && input.files[0]){
-					var reader = new FileReader();
+		async beforeMount() {
+		},
+		methods: {
+			onChangeStepImageFile(event, index) {
+				let input = event.target;
+				if (input.files && input.files[0]) {
+					let reader = new FileReader();
 					reader.onload = e => {
-						this.step1Image = e.target.result;
+						this.$set(this.sendData.stepImages, index, e.target.result)
 					}
 					reader.readAsDataURL(input.files[0]);
 				}
-				this.sendData.step1Image = this.$refs.step1Image.files[0];
+				this.stepImageFiles[index] = input.files[0]
 			},
-			onChangeStep2ImageFile(event) {
-				var input = event.target;
-				if(input.files && input.files[0]){
-					var reader = new FileReader();
-					reader.onload = e => {
-						this.step2Image = e.target.result;
-					}
-					reader.readAsDataURL(input.files[0]);
-				}
-				this.sendData.step2Image = this.$refs.step2Image.files[0];
-			},
-			onChangeStep3ImageFile(event) {
-				var input = event.target;
-				if(input.files && input.files[0]){
-					var reader = new FileReader();
-					reader.onload = e => {
-						this.step3Image = e.target.result;
-					}
-					reader.readAsDataURL(input.files[0]);
-				}
-				this.sendData.step3Image = this.$refs.step3Image.files[0];
-			},
-			onChangeStep4ImageFile(event) {
-				var input = event.target;
-				if(input.files && input.files[0]){
-					var reader = new FileReader();
-					reader.onload = e => {
-						this.step4Image = e.target.result;
-					}
-					reader.readAsDataURL(input.files[0]);
-				}
-				this.sendData.step4Image = this.$refs.step4Image.files[0];
-			},
-			onChangeStep5ImageFile(event) {
-				var input = event.target;
-				if(input.files && input.files[0]){
-					var reader = new FileReader();
-					reader.onload = e => {
-						this.step5Image = e.target.result;
-					}
-					reader.readAsDataURL(input.files[0]);
-				}
-				this.sendData.step5Image = this.$refs.step5Image.files[0];
-			},
+
+			submit() {
+				console.log(this.stepImageFiles)
+			}
 		}
 	}
 </script>
